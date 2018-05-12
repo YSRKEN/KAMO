6362979f1e21699e955e12beeace4306aeae9581
YSRKEN <ysr.ken@gmail.com>
2018-05-13 06:27:06 +0900

    基本的なUIパーツを追加

--- src/com/ysrken/kamo/Main.java
+++ src/com/ysrken/kamo/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
         primaryStage.setTitle("艦これモニタリングツール「KAMO」");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
     }
     public static void main(String[] args) {
