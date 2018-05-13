a84f0ecbff7bc80e29ebcea735da0aeb14d16422
YSRKEN <ysr.ken@gmail.com>
2018-05-13 10:17:32 +0900

    終了コマンドを実装した

--- src/com/ysrken/kamo/MainController.java
+++ src/com/ysrken/kamo/MainController.java
@@ -1,4 +1,15 @@
 package com.ysrken.kamo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 public class MainController {
+    @FXML private MenuItem ExitCommand;
+
+    public void initialize(){
+        // 終了コマンド
+        ExitCommand.setOnAction(e -> System.exit(0));
+
+    }
+
 }
