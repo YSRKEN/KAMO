97c19fcfb2bb33059f7b1e7d7cdda28e55de23ef
YSRKEN <ysr.ken@gmail.com>
2018-05-13 14:11:59 +0900

    Java8や　つわものどもが　ゆめのあと (日本語訳：愚直な方法以外で、「HTTPS形式のURLからテキストファイルをダウンロード」ができませんでした。 downloadTextDataメソッド以外のdownloadTextData2およびdownloadTextData3は失格です)

--- src/com/ysrken/kamo/MainController.java
+++ src/com/ysrken/kamo/MainController.java
@@ -3,21 +3,62 @@ package com.ysrken.kamo;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
 public class MainController {
     @FXML private MenuItem ExitMenu;
+    @FXML private MenuItem CheckVersionMenu;
     @FXML private MenuItem AboutMenu;
 
     public void initialize(){
         // 終了コマンド
         ExitMenu.setOnAction(e -> System.exit(0));
+        // バージョンチェックコマンド
+        CheckVersionMenu.setOnAction(e -> checkVersionCommand());
         // バージョン情報コマンド
         AboutMenu.setOnAction(e -> {
             String contentText = String.format("ソフト名：%s%nバージョン：%s%n作者：%s",
                     Utility.getSoftwareName(),
                     Utility.getSoftwareVersion(),
                     Utility.getSoftwareAuthor());
-            Utility.ShowDialog(contentText, "バージョン情報");
+            Utility.showDialog(contentText, "バージョン情報");
         });
+        // 起動時にバージョンチェックする
+        checkVersionCommand();
     }
 
+    // ソフトウェアの更新が来ているかをチェックする
+    private void checkVersionCommand(){
+        try {
+            // 更新情報を表すテキストファイルをダウンロードする
+            String checkText = Utility.downloadTextData("https://raw.githubusercontent.com/YSRKEN/KAMO/master/version.txt");
+            if(checkText == "")
+                throw new IOException();
+            // 更新文字列は「1,1.0.0」のような書式になっているはずなので確認する
+            String[] temp = checkText.split(",");
+            if(temp.length < 2){
+                throw new NumberFormatException();
+            }
+            // 情報を読み取っていく
+            int revision = Integer.parseInt(temp[0]);
+            if(Utility.getSoftwareRevision() < revision || true){
+                String message = String.format("より新しいバージョンが見つかりました。%n現行バージョン：%s%n最新バージョン：%s%nダウンロードサイトを開きますか？", Utility.getSoftwareVersion(), temp[1]);
+                boolean openUrlFlg = Utility.showChoiceDialog(message, "更新チェック");
+                if(openUrlFlg){
+                    Desktop desktop = Desktop.getDesktop();
+                    try{
+                        desktop.browse(new URI("https://github.com/YSRKEN/KAMO"));
+                    }catch( Exception e ){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }catch(NumberFormatException | IOException e){
+            e.printStackTrace();
+            Utility.showDialog("更新データを確認できませんでした。", "更新チェック");
+            return;
+        }
+    }
 }
--- src/com/ysrken/kamo/Utility.java
+++ src/com/ysrken/kamo/Utility.java
@@ -1,6 +1,17 @@
 package com.ysrken.kamo;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceDialog;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Utility {
     /**
@@ -57,11 +68,61 @@ public class Utility {
      * @param contentText 本文
      * @param headerText タイトル文
      */
-    public static void ShowDialog(String contentText, String headerText){
+    public static void showDialog(String contentText, String headerText){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
         alert.setTitle(SOFTWARE_NAME);
         alert.show();
+    }
+    public static boolean showChoiceDialog(String contentText, String headerText){
+        ChoiceDialog<Boolean> choice = new ChoiceDialog();
+        choice.setHeaderText(headerText);
+        choice.setContentText(contentText);
+        choice.setTitle(SOFTWARE_NAME);
+        return choice.showAndWait().orElse(false);
+    }
+    /**
+     * 指定したURL上のファイルをUTF-8形式の文字列としてダウンロードする。
+     * ダウンロードできなかった際は空文字列を返す
+     * @param url URL
+     * @return UTF-8形式のテキストデータ
+     */
+    public static String downloadTextData(String url){
+        try(InputStream is = new URL(url).openStream();
+            InputStreamReader isr = new InputStreamReader(is, Charset.forName("UTF-8"));
+            BufferedReader br = new BufferedReader(isr)){
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+    public static String downloadTextData2(String url){
+        try (Stream<String> a = Files.lines(Paths.get(new URI(url)), Charset.forName("UTF-8"))) {
+            return a.collect(Collectors.joining());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+    public static String downloadTextData3(String url){
+        try {
+            return new String(Files.readAllBytes(Paths.get(new URI(url))), Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
