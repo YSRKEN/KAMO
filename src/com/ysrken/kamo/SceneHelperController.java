fd90ec1835ee8ea8fc1d63bee7ad668dff8ae6e9
YSRKEN <ysr.ken@gmail.com>
2018-05-26 13:04:50 +0900

    画像認識画面について、ModelとControllerを分離するようにした

--- src/com/ysrken/kamo/SceneHelperController.java
+++ src/com/ysrken/kamo/SceneHelperController.java
@@ -9,30 +9,38 @@ import javafx.scene.layout.BorderPane;
 import java.awt.image.BufferedImage;
 
 public class SceneHelperController {
+    /**
+     * 各コントロール・コンテナ
+     */
     @FXML private ImageView SceneImageView;
     @FXML private BorderPane SceneBP;
     @FXML private Label DifferenceHashLabel;
     @FXML private Label AverageColorLabel;
 
+    /**
+     * Model
+     */
     private SceneHelperModel model;
 
     /**
      * 初期化
      */
     public void initialize(){
-        // ImageViewのサイズを自動調整する。
+        model = new SceneHelperModel();
+        // Data Binding
+        SceneImageView.imageProperty().bind(model.ViewImage);
+        DifferenceHashLabel.textProperty().bind(model.DifferenceHash);
+        AverageColorLabel.textProperty().bind(model.AverageColor);
+        // ImageViewのサイズを自動調整する
         // 参考→https://qiita.com/opengl-8080/items/29c3ef163f41ee172173
         SceneImageView.fitWidthProperty().bind(SceneBP.widthProperty());
         SceneImageView.fitHeightProperty().bind(SceneBP.heightProperty());
-
-        DifferenceHashLabel.setText("テスト1");
-        AverageColorLabel.setText("テスト2");
     }
     /**
      * 画像をセット
      * @param image 画像データ
      */
     public void setImage(BufferedImage image){
-        SceneImageView.setImage(SwingFXUtils.toFXImage(image, null));
+        model.setImage(image);
     }
 }
--- src/com/ysrken/kamo/SceneHelperModel.java
+++ src/com/ysrken/kamo/SceneHelperModel.java
@@ -1,5 +1,41 @@
 package com.ysrken.kamo;
 
-public class SceneHelperModel {
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
 
+import java.awt.image.BufferedImage;
+
+public class SceneHelperModel {
+    /**
+     * 表示用画像データ
+     */
+    public final ObjectProperty<Image> ViewImage = new SimpleObjectProperty<>(null);
+    public final StringProperty DifferenceHash = new SimpleStringProperty("テスト1");
+    public final StringProperty AverageColor = new SimpleStringProperty("テスト2");
+
+    /**
+     * 内部用画像データ
+     */
+    private final ObjectProperty<BufferedImage> viewImageB = new SimpleObjectProperty<>(null);
+
+    /**
+     * コンストラクタ
+     */
+    public SceneHelperModel(){
+        // ChangeListenerを設定
+        viewImageB.addListener((observable, oldValue, newValue) -> {
+            ViewImage.set(SwingFXUtils.toFXImage(newValue, null));
+        });
+    }
+    /**
+     * 画像データをセット
+     * @param image 画像データ
+     */
+    public void setImage(BufferedImage image){
+        viewImageB.set(image);
+    }
 }
