197dce65b580364d4b6b34afb695312edfdf878e
YSRKEN <ysr.ken@gmail.com>
2018-05-27 02:03:13 +0900

    カスタムコントロールを実装することで、コードおよびFXMLを大幅に簡略化した

--- src/com/ysrken/kamo/Controller/BattleSceneReflectionController.java
+++ src/com/ysrken/kamo/Controller/BattleSceneReflectionController.java
@@ -7,96 +7,41 @@ import javafx.beans.property.StringProperty;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
 import java.awt.image.BufferedImage;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class BattleSceneReflectionController {
-    // RI KI WA ZA
-    @FXML private BorderPane SceneLabel1BP;
-    @FXML private BorderPane SceneLabel2BP;
-    @FXML private BorderPane SceneLabel3BP;
-    @FXML private BorderPane SceneLabel4BP;
-    @FXML private BorderPane SceneLabel5BP;
-    @FXML private ImageView SceneImageView1;
-    @FXML private ImageView SceneImageView2;
-    @FXML private ImageView SceneImageView3;
-    @FXML private ImageView SceneImageView4;
-    @FXML private ImageView SceneImageView5;
-    @FXML private Label SceneLabel1;
-    @FXML private Label SceneLabel2;
-    @FXML private Label SceneLabel3;
-    @FXML private Label SceneLabel4;
-    @FXML private Label SceneLabel5;
-
     /**
-     * シーンごとのマッピング
+     * タブ一覧(コントロール)
      */
-    private final Map<String, TabContents> sceneMapping = new HashMap<>();
-
+    @FXML private TabPane SceneTabs;
     /**
-     * タブに表示する情報
+     * 表示するシーン一覧
      */
-    private class TabContents{
-        private ObjectProperty<Image> tabImage = new SimpleObjectProperty<>(null);
-        private StringProperty tabLabel = new SimpleStringProperty("");
-        private ImageView imageView;
-        private Label label;
-        /**
-         * コンストラクタ
-         * @param imageView 取得画像を表示するImageView
-         * @param label 取得時刻を表示するLabel
-         */
-        public TabContents(ImageView imageView, Label label){
-            this.imageView = imageView;
-            this.label = label;
-            this.imageView.imageProperty().bind(tabImage);
-            this.label.textProperty().bind(tabLabel);
-        }
-
-        /**
-         * 画像をセットする
-         * @param image BufferedImage形式の画像
-         */
-        public void setImage(BufferedImage image){
-            tabImage.set(SwingFXUtils.toFXImage(image, null));
-        }
-        /**
-         * 文字列をセットする
-         * @param text String型の文字列
-         */
-        public void setText(String text){
-            tabLabel.set(text);
-        }
-    }
+    final private Set<String> sceneList = new LinkedHashSet<>(
+            Arrays.asList("昼戦後", "夜戦後", "戦闘結果", "MVP", "マップ")
+    );
+    /**
+     * タブ一覧(ハッシュアクセス用)
+     */
+    private Map<String, SceneTab> tabMap = new HashMap<>();
 
     /**
      * 初期化
      */
     public void initialize(){
-        // ImageViewのサイズを自動調整する。
-        // 参考→https://qiita.com/opengl-8080/items/29c3ef163f41ee172173
-        SceneImageView1.fitWidthProperty().bind(SceneLabel1BP.widthProperty());
-        SceneImageView1.fitHeightProperty().bind(SceneLabel1BP.heightProperty());
-        SceneImageView2.fitWidthProperty().bind(SceneLabel2BP.widthProperty());
-        SceneImageView2.fitHeightProperty().bind(SceneLabel2BP.heightProperty());
-        SceneImageView3.fitWidthProperty().bind(SceneLabel3BP.widthProperty());
-        SceneImageView3.fitHeightProperty().bind(SceneLabel3BP.heightProperty());
-        SceneImageView4.fitWidthProperty().bind(SceneLabel4BP.widthProperty());
-        SceneImageView4.fitHeightProperty().bind(SceneLabel4BP.heightProperty());
-        SceneImageView5.fitWidthProperty().bind(SceneLabel5BP.widthProperty());
-        SceneImageView5.fitHeightProperty().bind(SceneLabel5BP.heightProperty());
-        // マッピング
-        sceneMapping.put("昼戦後",   new TabContents(SceneImageView1, SceneLabel1));
-        sceneMapping.put("夜戦後",   new TabContents(SceneImageView2, SceneLabel2));
-        sceneMapping.put("戦闘結果", new TabContents(SceneImageView3, SceneLabel3));
-        sceneMapping.put("MVP",     new TabContents(SceneImageView4, SceneLabel4));
-        sceneMapping.put("マップ",   new TabContents(SceneImageView5, SceneLabel5));
+        // SceneTabを自動生成する
+        final var tabList = SceneTabs.getTabs();
+        for(var scene : sceneList) {
+            final var sceneTab = new SceneTab(scene);
+            tabList.add(sceneTab);
+            tabMap.put(scene, sceneTab);
+        }
     }
     /**
      * 画像をタブにセット
@@ -104,7 +49,8 @@ public class BattleSceneReflectionController {
      * @param image 画像
      */
     public void setImage(String key, BufferedImage image){
-        sceneMapping.get(key).setImage(image);
+        final var targetTab = tabMap.get(key);
+        targetTab.setImage(image);
     }
     /**
      * テキストをタブにセット
@@ -112,9 +58,14 @@ public class BattleSceneReflectionController {
      * @param text テキスト
      */
     public void setText(String key, String text){
-        sceneMapping.get(key).setText(text);
+        final var targetTab = tabMap.get(key);
+        targetTab.setLabelText(text);
     }
+    /**
+     * 表示するシーン一覧
+     * @return 表示するシーン一覧
+     */
     public Set<String> getBattleSceneSet(){
-        return sceneMapping.keySet();
+        return sceneList;
     }
 }
---
+++ src/com/ysrken/kamo/Controller/SceneTab.java
@@ -0,0 +1,58 @@
+package com.ysrken.kamo.Controller;
+
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.Buffer;
+
+/**
+ * タブによるスクショ表示を表現するカスタムコントロール
+ * 参考→
+ * 「FXMLを利用する（２）リソースバンドル／カスタムコンポーネント」
+ * (URL略)
+ * 「JavaFX8で簡単なアプリケーションを簡単に作る方法」
+ * (http://seraphy.hatenablog.com/entries/2015/04/11)
+ */
+public class SceneTab extends Tab {
+    @FXML private BorderPane SceneBP;
+    @FXML private Label SceneLabel;
+    @FXML private ImageView SceneImageView;
+    /**
+     * コンストラクタ
+     */
+    public SceneTab(String scene){
+        super();
+        this.setText(scene);
+        final var fxmlLoader  = new FXMLLoader(getClass().getResource("../View/SceneTab.fxml"));
+        // FXMLのルートタグとして、自らを登録
+        fxmlLoader.setRoot( this );
+        fxmlLoader.setController( this );
+        // FXMLの読込
+        try{
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+        // ImageViewのサイズを自動調整する
+        // 参考→https://qiita.com/opengl-8080/items/29c3ef163f41ee172173
+        SceneImageView.fitWidthProperty().bind(SceneBP.widthProperty());
+        SceneImageView.fitHeightProperty().bind(SceneBP.heightProperty());
+    }
+    public void setImage(BufferedImage image){
+        Platform.runLater(() -> SceneImageView.setImage(SwingFXUtils.toFXImage(image, null)));
+    }
+    public void setLabelText(String text){
+        Platform.runLater(() -> SceneLabel.setText(text));
+    }
+}
