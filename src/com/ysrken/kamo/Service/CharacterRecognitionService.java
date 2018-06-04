ef56b0197c354a981e409ec4c6d4189f91349c6a
YSRKEN <ysr.ken@gmail.com>
2018-06-04 20:57:44 +0900

    画像を自動的に切り出すルーチンまで実装した

--- src/com/ysrken/kamo/Service/CharacterRecognitionService.java
+++ src/com/ysrken/kamo/Service/CharacterRecognitionService.java
@@ -1,25 +1,26 @@
 package com.ysrken.kamo.Service;
 
 import javax.imageio.ImageIO;
+import java.util.Arrays;
 import java.util.List;
 import java.awt.*;
 import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorConvertOp;
-import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.stream.Collector;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static java.awt.Image.SCALE_SMOOTH;
 
 public class CharacterRecognitionService {
     /** 文字認識用に引き伸ばす縦幅 */
-    private static int ocrStretchHeight = 128;
+    private static int ocrStretchHeight1 = 64;
+    /** 文字認識用に引き伸ばす縦幅 */
+    private static int ocrStretchHeight2 = 32;
 
     /**
      * ％表記の割合(A)と100％時のピクセル値(B)から、ピクセルを出力する
@@ -32,6 +33,27 @@ public class CharacterRecognitionService {
         final var rawPixel = per * pixel / 100;
         final var roundPixel = Math.round(rawPixel);
         return Math.min(Math.max(roundPixel, 0), pixel - 1);
+    }
+
+    /** 周囲から最小サイズを切り出すためにRectを割り出す */
+    private static Rectangle getTrimmingRect(BufferedImage image){
+        // 水平方向から縦に見たヒストグラム
+        final var blackCountWidth = IntStream.range(0, image.getWidth()).map(x -> {
+            return  (int)IntStream.range(0, image.getHeight()).filter(y -> (image.getRGB(x, y) & 0xFF) == 0).count();
+        }).toArray();
+        // 垂直方向から横に見たヒストグラム
+        final var blackCountHeight = IntStream.range(0, image.getHeight()).map(y -> {
+            return  (int)IntStream.range(0, image.getWidth()).filter(x -> (image.getRGB(x, y) & 0xFF) == 0).count();
+        }).toArray();
+        // Rectを割り出す
+        final var rect = new Rectangle(0, 0, image.getWidth(), image.getHeight());
+        rect.x = IntStream.range(0, image.getWidth()).filter(i -> blackCountWidth[i] != 0).findFirst().orElse(0);
+        rect.y = IntStream.range(0, image.getHeight()).filter(i -> blackCountHeight[i] != 0).findFirst().orElse(0);
+        rect.width = IntStream.range(0, image.getWidth()).map(i -> image.getWidth() - i - 1).filter(i -> blackCountWidth[i] != 0)
+                .findFirst().orElse(image.getWidth() - 1) - rect.x + 1;
+        rect.height = IntStream.range(0, image.getHeight()).map(i -> image.getHeight() - i - 1).filter(i -> blackCountHeight[i] != 0)
+                .findFirst().orElse(image.getHeight() - 1) - rect.y + 1;
+        return rect;
     }
 
     /**
@@ -55,7 +77,7 @@ public class CharacterRecognitionService {
         // java.awt.Image
         final var tempImage1 = image
                     .getSubimage(rectX, rectY, rectW, rectH)
-                    .getScaledInstance(-1, ocrStretchHeight, SCALE_SMOOTH);
+                    .getScaledInstance(-1, ocrStretchHeight1, SCALE_SMOOTH);
         final var tempImage2 = new BufferedImage(tempImage1.getWidth(null), tempImage1.getHeight(null), image.getType());
         tempImage2.getGraphics().drawImage(tempImage1, 0, 0, null);
         final var colorConvert = new ColorConvertOp(
@@ -107,9 +129,25 @@ public class CharacterRecognitionService {
             xBegin = xEnd;
         }
         // それぞれの数値を読み取る
-        final var digit = new int[6];
+        final var digit = IntStream.range(0, 6).map(i -> i == 5 ? -1 : 0).toArray();
         if(splitRectList.size() == 8){
-
+            final var indexList = new int[]{0, 1, 3, 4, 6, 7};
+            for(int i = 0; i < indexList.length; ++i){
+                final var rect = splitRectList.get(indexList[i]);
+                final var cropImage = tempImage4.getSubimage(rect.x, rect.y, rect.width, rect.height);
+                try {
+                    ImageIO.write(cropImage, "png", new File("temp3-" + (i + 1) + "-1.png"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                final var cropRect = getTrimmingRect(cropImage);
+                final var cropImage2 = cropImage.getSubimage(cropRect.x, cropRect.y, cropRect.width, cropRect.height);
+                try {
+                    ImageIO.write(cropImage2, "png", new File("temp3-" + (i + 1) + "-2.png"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return digit;
     }
