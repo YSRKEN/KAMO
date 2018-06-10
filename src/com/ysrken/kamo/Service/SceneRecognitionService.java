995066a7f7a58209763b9d9d5206582de64c17fc
YSRKEN <ysr.ken@gmail.com>
2018-06-10 14:32:34 +0900

    SceneRecognitionServiceも対応させた

--- src/com/ysrken/kamo/Service/SceneRecognitionService.java
+++ src/com/ysrken/kamo/Service/SceneRecognitionService.java
@@ -1,5 +1,6 @@
 package com.ysrken.kamo.Service;
 
+import com.ysrken.kamo.BitmapImage;
 import com.ysrken.kamo.Utility;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
@@ -20,13 +21,9 @@ import java.util.stream.Collectors;
 
 import static java.awt.Image.SCALE_SMOOTH;
 
-/**
- * 画面のシーン判定を行う
- */
+/** 画面のシーン判定を行う */
 public class SceneRecognitionService {
-    /**
-     * 画像がそのシーンたりうる証拠
-     */
+    /** 画像がそのシーンたりうる証拠 */
     static interface SceneEvidence{
         /**
          * 画像がその証拠とマッチするかを判定する
@@ -35,80 +32,10 @@ public class SceneRecognitionService {
          */
         public boolean isMatchImage(BufferedImage image);
     }
-    /**
-     * 画像がそのシーンたりうる証拠(DifferenceHash版)
-     */
+    /** 画像がそのシーンたりうる証拠(DifferenceHash版 */
     static class SceneEvidenceDH implements SceneEvidence{
         private double xPer, yPer, wPer, hPer;
         private long hash;
-        /**
-         * ％表記の割合(A)と100％時のピクセル値(B)から、ピクセルを出力する
-         * ただし出力値は、[0, B - 1]にクロップされる
-         * @param per 割合
-         * @param pixel ピクセル
-         * @return 割合値のピクセル
-         */
-        private long perToPixel(double per, int pixel){
-            final var rawPixel = per * pixel / 100;
-            final var roundPixel = Math.round(rawPixel);
-            return Math.min(Math.max(roundPixel, 0), pixel - 1);
-        }
-        /**
-         * 画像の一部分におけるDifferenceHashを取得する(rectで指定する範囲は％単位)
-         * @param image 画像
-         * @return ハッシュ値
-         */
-        private long calcDifferenceHash(BufferedImage image) {
-            // 画像の選択範囲(％)を選択範囲(ピクセル)に変換
-            final var rectX = (int)perToPixel(xPer, image.getWidth());
-            final var rectY = (int)perToPixel(yPer, image.getHeight());
-            final var rectW = (int)perToPixel(wPer, image.getWidth());
-            final var rectH = (int)perToPixel(hPer, image.getHeight());
-            // 元画像を切り抜き、9x8ピクセルにリサイズ
-            final var tempImage = image
-                    .getSubimage(rectX, rectY, rectW, rectH)
-                    .getScaledInstance(9, 8, SCALE_SMOOTH);
-            // リサイズした画像を、グレースケール化しつつBufferedImageに書き出し
-            final var canvas = new BufferedImage(9, 8, BufferedImage.TYPE_BYTE_GRAY);
-            final var g = canvas.getGraphics();
-            g.drawImage(tempImage, 0, 0, null);
-            g.dispose();
-            // 隣接ピクセルとの比較結果を符号化する
-            long hash = 0;
-            for (var y = 0; y < 8; ++y){
-                for (var x = 0; x < 8; ++x){
-                    hash <<= 1;
-                    final var b1 = canvas.getRGB(x, y) & 0xFF;
-                    final var b2 = canvas.getRGB(x + 1, y) & 0xFF;
-                    if (b1 > b2)
-                        hash |= 1;
-                }
-            }
-            return hash;
-        }
-        /**
-         * ビットカウント
-         * 参考→http://developer.cybozu.co.jp/takesako/2006/11/binary_hacks.html
-         * @param x long型(64bit)の値
-         * @return ビットカウント後の数
-         */
-        private long popcnt(long x) {
-            x = ((x & 0xaaaaaaaaaaaaaaaaL) >> 1) + (x & 0x5555555555555555L);
-            x = ((x & 0xccccccccccccccccL) >> 2) + (x & 0x3333333333333333L);
-            x = ((x & 0xf0f0f0f0f0f0f0f0L) >> 4) + (x & 0x0f0f0f0f0f0f0f0fL);
-            x = ((x & 0xff00ff00ff00ff00L) >> 8) + (x & 0x00ff00ff00ff00ffL);
-            x = ((x & 0xffff0000ffff0000L) >> 16) + (x & 0x0000ffff0000ffffL);
-            x = ((x & 0xffffffff00000000L) >> 32) + (x & 0x00000000ffffffffL);
-            return x;
-        }
-        /**
-         * hashｔのハミング距離を計算する
-         * @param a 値
-         * @return ハミング距離
-         */
-        private long calcHummingDistance(long a) {
-            return popcnt(a ^ hash);
-        }
         /**
          * コンストラクタ
          * @param xPer 左上X座標の％
@@ -130,70 +57,14 @@ public class SceneRecognitionService {
          * @return マッチすればtrue
          */
         public boolean isMatchImage(BufferedImage image){
-            final var hash = calcDifferenceHash(image);
-            //System.out.println(hash);
-            return calcHummingDistance(hash) < 20;
+            final var hash = BitmapImage.of(image).calcDifferenceHash(xPer, yPer, wPer, hPer);
+            return Utility.calcHummingDistance(this.hash, hash) < 20;
         }
     }
-    /**
-     * 画像がそのシーンたりうる証拠(AverageColor版)
-     */
+    /** 画像がそのシーンたりうる証拠(AverageColor版) */
     static class SceneEvidenceAC implements SceneEvidence{
         private double xPer, yPer, wPer, hPer;
-        private int r, g, b;
-        /**
-         * ％表記の割合(A)と100％時のピクセル値(B)から、ピクセルを出力する
-         * ただし出力値は、[0, B - 1]にクロップされる
-         * @param per 割合
-         * @param pixel ピクセル
-         * @return 割合値のピクセル
-         */
-        private long perToPixel(double per, int pixel){
-            final var rawPixel = per * pixel / 100;
-            final var roundPixel = Math.round(rawPixel);
-            return Math.min(Math.max(roundPixel, 0), pixel - 1);
-        }
-        /**
-         * 画像の一部分における平均色を取得する(rectで指定する範囲は％単位)
-         * @param image 画像
-         * @return 平均色
-         */
-        private Color calcAverageColor(BufferedImage image){
-            // 画像の選択範囲(％)を選択範囲(ピクセル)に変換
-            final var rectX = (int)perToPixel(xPer, image.getWidth());
-            final var rectY = (int)perToPixel(yPer, image.getHeight());
-            final var rectW = (int)perToPixel(wPer, image.getWidth());
-            final var rectH = (int)perToPixel(hPer, image.getHeight());
-            // 画素値の平均色を計算する
-            long rSum = 0, gSum = 0, bSum = 0;
-            for(int y = rectY; y < rectY + rectH; ++y) {
-                for (int x = rectX; x < rectX + rectW; ++x) {
-                    final var color = image.getRGB(x, y);
-                    rSum += (color >>> 16) & 0xFF;
-                    gSum += (color >>> 8) & 0xFF;
-                    bSum += color & 0xFF;
-                }
-            }
-            int rAve = (int)Math.round(1.0 * rSum / rectW / rectH);
-            int gAve = (int)Math.round(1.0 * gSum / rectW / rectH);
-            int bAve = (int)Math.round(1.0 * bSum / rectW / rectH);
-            // クロップ後に出力
-            rAve = (rAve < 0 ? 0 : rAve > 255 ? 255 : rAve);
-            gAve = (gAve < 0 ? 0 : gAve > 255 ? 255 : gAve);
-            bAve = (bAve < 0 ? 0 : bAve > 255 ? 255 : bAve);
-            return new Color(rAve, gAve, bAve);
-        }
-        /**
-         * 色間のRGB色空間における距離を計算する
-         * @param a 色
-         * @return 距離
-         */
-        private int calcColorDistance(Color a){
-            final var rDiff = a.getRed() - r;
-            final var gDiff = a.getGreen() - g;
-            final var bDiff = a.getBlue() - b;
-            return rDiff * rDiff + gDiff * gDiff + bDiff * bDiff;
-        }
+        private Color color;
         /**
          * コンストラクタ
          * @param xPer 左上X座標の％
@@ -209,9 +80,7 @@ public class SceneRecognitionService {
             this.yPer = yPer;
             this.wPer = wPer;
             this.hPer = hPer;
-            this.r = r;
-            this.g = g;
-            this.b = b;
+            this.color = new Color(r, g, b);
         }
         /**
          * 画像がその証拠とマッチするかを判定する
@@ -219,21 +88,16 @@ public class SceneRecognitionService {
          * @return マッチすればtrue
          */
         public boolean isMatchImage(BufferedImage image){
-            final var color = calcAverageColor(image);
-            //System.out.println("" + color.getRed() + "," + color.getGreen() + "," + color.getBlue());
-            return calcColorDistance(color) < 50;
+            final var color = BitmapImage.of(image).calcAverageColor(xPer, yPer, wPer, hPer);
+            return Utility.calcColorDistance(this.color, color) < 50;
         }
     }
 
-    /**
-     * シーン一覧
-     */
+    /** シーン一覧 */
     private static Map<String, SceneEvidence[]> sceneList = new HashMap<String, SceneEvidence[]>();
     private static SceneEvidence[] nearlyHomeScene = new SceneEvidence[]{};
 
-    /**
-     * 初期化コード
-     */
+    /** 初期化コード */
     public static void initialize(){
         // JSONを読み込む
         // 参考→https://symfoware.blog.fc2.com/blog-entry-2094.html
