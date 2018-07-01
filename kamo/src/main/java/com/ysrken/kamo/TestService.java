39fdc690a215a0e827201072b199ff8164b3652b
YSRKEN <ysr.ken@gmail.com>
2018-07-01 21:11:00 +0900

    DI実装実験に向け、メソッド周りを整理した

---
+++ kamo/src/main/java/com/ysrken/kamo/TestService.java
@@ -0,0 +1,30 @@
+package com.ysrken.kamo;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+
+public class TestService {
+	public String joinName(String firstName, String lastName, Logger log) {
+        StringBuilder builder = new StringBuilder();
+
+        if (!StringUtils.isEmpty(firstName)) {
+            builder.append(firstName);
+        }
+
+        if (!StringUtils.isEmpty(lastName)) {
+            if (builder.length() > 0) {
+                builder.append(" ");
+            }
+            builder.append(lastName);
+        }
+
+        if (builder.length() > 0) {
+            String name = builder.toString();
+            log.debug("Saying hello to " + name);
+            return "Hello " + name;
+        } else {
+            log.debug("Neither first name nor last name was set, saying hello to anonymous person");
+            return "Hello mysterious person";
+        }
+	}
+}
