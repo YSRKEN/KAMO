26b7bb3877f9d14e8bdda04d82639175f6614006
YSRKEN <ysr.ken@gmail.com>
2019-01-14 12:52:47 +0900

    バージョンアップ

--- kamo/src/main/java/com/ysrken/kamo/Constant.java
+++ kamo/src/main/java/com/ysrken/kamo/Constant.java
@@ -13,12 +13,12 @@ public class Constant {
     /**
      * ソフトウェアのバージョン番号
      */
-    public static final String SOFTWARE_VER = "2.3.0";
+    public static final String SOFTWARE_VER = "2.4.0";
     
     /**
      * ソフトウェアのリビジョン番号
      */
-    public static final int SOFTWARE_REVISION = 10;
+    public static final int SOFTWARE_REVISION = 11;
 
     /**
      * ソフトウェアの作者名
